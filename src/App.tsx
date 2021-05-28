48371aa2f72378e070b363c5b7679ca1e008d37e
Cristian <cristian.tirdad@gmail.com>
2021-05-28 22:36:16 +0200

    Install Ant Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Calendar from "./components/Calendar";
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello world</h1>
+      <Calendar />
     </div>
   );
 }
